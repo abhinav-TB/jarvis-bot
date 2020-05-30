87e2ebb22aa8769d3f67e4a195db530d122c28db
abhinav-TB <abhinavtb@gmail.com>
2020-05-30 15:10:07 +0530

    created main

--- jarvis.py
+++ jarvis.py
@@ -12,21 +12,21 @@ def speak(audio):
 
 def time():
     Time =datetime.datetime.now().strftime("%I:%M:%S")
+    speak("the current time is")
     speak(Time)
 
 def date():
     year =int(datetime.datetime.now().year)
     month =int(datetime.datetime.now().month)
     date =int(datetime.datetime.now().day)
+    speak("the current date is")
     speak(date)
     speak(month)
     speak(year)
 
 def wishme():
     speak("welcome back sir")
-    speak("the current time is")
     time()
-    speak("the current date is")
     date()
     hour =datetime.datetime.now().hour
     if hour>= 6 and  hour<12:
@@ -59,7 +59,15 @@ def takeCommand():
 
     return query
 
-takeCommand()
+if __name__ == "__main__":
+    wishme()
+    while True:
+        query =takeCommand().lower()
+
+        if 'time' in query:
+            time()
+        elif 'date' in query:
+            date()
 
         
 
